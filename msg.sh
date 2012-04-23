998e555e7d2977f4a174ec8687d822c45c97dfe1
Dennis Ideler <ideler.dennis@gmail.com>
2012-04-23 03:10:41 -0400

    Add script for a simple instant chat across the network

---
+++ msg.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Write a message to someone on the network.
+# 
+#   [di07ty@sandcastle ~]$ ./msg.sh di07ty This is a test. How are you?
+#
+#   Message from di07ty@sandcastle.cosc.BrockU.CA on pts/14 at 03:04 ...
+#   "This is a test. How are you?"
+#   EOF
+#
+# Copyright 2012 Dennis Ideler
+#
+# "shift" gets rid of parameter $1.
+# "@" contains all of the remaining positional parameters as a quoted string.
+
+USER="$1"
+shift
+write $USER << EOF
+"$@"
+EOF
